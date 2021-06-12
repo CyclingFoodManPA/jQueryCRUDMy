aaae540f4026c425a5c0c0d0170fdec02c528c2e
CyclingFoodmanPA <ks.safford@verizon.net>
2021-06-12 13:38:30 -0400

    Made numerous changes per the article on DotNetTricks.

--- jQueryCRUDMy/App_Start/RouteConfig.cs
+++ jQueryCRUDMy/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace jQueryCRUDMy {
@@ -14,7 +10,7 @@ namespace jQueryCRUDMy {
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new {
-                    controller = "Home",
+                    controller = "User",
                     action = "Index",
                     id = UrlParameter.Optional
                 }
---
+++ jQueryCRUDMy/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using System.Data.Entity;
+using jQueryCRUDMy.Context;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace jQueryCRUDMy.Controllers {
+    public class UserController : Controller {
+        private readonly DataContext _db = new DataContext();
+
+        // GET: User
+        public ActionResult Index() {
+            return View(_db.User.ToList());
+        }
+
+        //
+        // GET: /User/Details/5
+
+        public ActionResult Details(int id = 0) {
+            User user = _db.User.Find(id);
+            if (user == null) {
+                return HttpNotFound();
+            }
+            return PartialView(user);
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create() {
+            return PartialView();
+        }
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        public ActionResult Create(User user) {
+            if (ModelState.IsValid) {
+                _db.User.Add(user);
+                _db.SaveChanges();
+                TempData["Message"] = "Data has been saved successfully!";
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        //
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(int id = 0) {
+            User user = _db.User.Find(id);
+            if (user == null) {
+                return HttpNotFound();
+            }
+            return PartialView(user);
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(User user) {
+            if (ModelState.IsValid) {
+                _db.Entry(user).State = EntityState.Modified;
+                _db.SaveChanges();
+                TempData["Message"] = "Data has been updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        //
+        // GET: /User/Delete/5
+
+        public ActionResult Delete(int id = 0) {
+            User user = _db.User.Find(id);
+            _db.User.Remove(user);
+            _db.SaveChanges();
+            TempData["Message"] = "Data has been deleted successfully!";
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing) {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
